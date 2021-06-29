93a9acd92a5f7002d7bc16e2018b1faa620b21a1
joaquinrevi <joaquinrevi@hotmail.com>
2021-06-29 15:27:13 -0300

    Solucion de error

--- ManejadorDeArchivos/Properties/Resources.Designer.cs
+++ ManejadorDeArchivos/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace ManejadorDeArchivos.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap _720115 {
-            get {
-                object obj = ResourceManager.GetObject("720115", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
